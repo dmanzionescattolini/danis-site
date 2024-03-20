445917050472c3bab6198e64f0569293f677bb2f
Donato Manzione <donato.manz.sc@gmail.com>
2024-03-20 05:52:35 -0400

    m

--- components/Video Gallery/VideoGallery2.jsx
+++ components/Video Gallery/VideoGallery2.jsx
@@ -2,40 +2,40 @@
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import 'mdb-react-ui-kit/dist/mdb-react-ui-kit.cjs'
 import 'bootstrap/dist/css/bootstrap.css';
-import { MDBListGroup, MDBListGroupItem, MDBNavbar, MDBNavbarLink } from "mdb-react-ui-kit";
+import { MDBListGroup, MDBListGroupItem, MDBNavbar } from "mdb-react-ui-kit";
 import { useState } from "react";
 
 export default function VideoGallery() {
     const [activeVideo, setActiveVideo] = useState("https://pagina-mama.s3.amazonaws.com/assets2/daniel/All+Clips.mp4");
     return <div id="video-gallery" className="w-100 h-100 m-0 p-0 object-fit-contain m-0 p-0">
-        <div className="mask ng-dark-subtle opacity-25 w-100 h-auto embed-responsive-container" ></div>
+        <div className="mask bg-light-subtle opacity-25 w-100 h-auto" ></div>
         
         <video autoPlay playsInline loop muted id={"w-auto h-100"} src={activeVideo} className="min-vw-100 min-vh-100 h-auto w-100 z-0 embed-responsive"
             alt="Three Bullets for Bombay" />;
-        <MDBNavbar fluid className={`position-fixed z-1`} dir='vertical'>
+        <MDBNavbar id="films-menu"fluid className={`position-fixed z-1`} dir='vertical'>
             <MDBListGroup id="films-menu"
                 className="list-group-light text-white border-0 bg-transparent vh-100 justify-content-end text-indent">
                 <MDBListGroupItem className={"list-group-item text-white border-0 bg-transparent"}>
-                    <MDBNavbarLink href="/films/rackets-all-the-way-down" onMouseLeave={() => setActiveVideo("https://pagina-mama.s3.amazonaws.com/assets2/daniel/All+Clips.mp4")} onMouseEnter={() =>
-                        setActiveVideo("https://pagina-mama.s3.amazonaws.com/assets2/daniel/Rackets+All+the+Way+Down/Still.png")}>
+                    <a href="/films/rackets-all-the-way-down" onMouseLeave={() => setActiveVideo("https://pagina-mama.s3.amazonaws.com/assets2/daniel/All+Clips.mp4")} onMouseEnter={() =>
+                        setActiveVideo("https://pagina-mama.s3.amazonaws.com/assets2/daniel/Rackets+All+the+Way+Down/Rackets+Clip.mp4")}>
                         Rackets All the Way Down
-                    </MDBNavbarLink> </MDBListGroupItem>
+                    </a> </MDBListGroupItem>
                 <MDBListGroupItem className={"list-group-item text-white border-0 bg-transparent"}>
-                    <MDBNavbarLink onMouseLeave={() => setActiveVideo("https://pagina-mama.s3.amazonaws.com/assets2/daniel/All+Clips.mp4")} onMouseEnter={() => setActiveVideo("https://pagina-mama.s3.amazonaws.com/assets2/daniel/The+Wrench/The+wrench+clip.mp4")}
+                    <a onMouseLeave={() => setActiveVideo("https://pagina-mama.s3.amazonaws.com/assets2/daniel/All+Clips.mp4")} onMouseEnter={() => setActiveVideo("https://pagina-mama.s3.amazonaws.com/assets2/daniel/The+Wrench/The+wrench+clip.mp4")}
                         href="/films/the-wrench">
                         The Wrench
-                    </MDBNavbarLink>
+                    </a>
                 </MDBListGroupItem>
                 <MDBListGroupItem className={"list-group-item text-white border-0 bg-transparent"}>
-                    <MDBNavbarLink
+                    <a
                         onMouseLeave={() => setActiveVideo("https://pagina-mama.s3.amazonaws.com/assets2/daniel/All+Clips.mp4")} onMouseEnter={() => setActiveVideo("https://pagina-mama.s3.amazonaws.com/assets2/daniel/A+Pandemonic+Serenade/APS+Clip.mp4")} href="/films/a-pandemonic-serenade"> A Pandemonic
-                        Serenade </MDBNavbarLink> </MDBListGroupItem>
+                        Serenade </a> </MDBListGroupItem>
                 <MDBListGroupItem className={"list-group-item text-white border-0 bg-transparent"}>
 
-                    <MDBNavbarLink
+                    <a
                         href="/films/three-bullets-for-bombay" onMouseLeave={() => setActiveVideo("https://pagina-mama.s3.amazonaws.com/assets2/daniel/All+Clips.mp4")} onMouseEnter={() => setActiveVideo("https://pagina-mama.s3.amazonaws.com/assets2/daniel/Three+Bullets+to+Bombay+Beach/3BBB+Clip.mp4")}>
                         Three Bullets to Bombay Beach
-                    </MDBNavbarLink>
+                    </a>
                 </MDBListGroupItem>
             </MDBListGroup>
         </MDBNavbar>
