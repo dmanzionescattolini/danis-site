10fe22915af85ab9a4c16fdf261cb05c95c85850
Donato Manzione <donato.manz.sc@gmail.com>
2024-04-01 19:09:09 -0400

    m

--- components/Home/Home.jsx
+++ components/Home/Home.jsx
@@ -20,8 +20,8 @@ export default function Home() {
   //     ,100);
 
   useEffect(() => {
-    document.getElementById("toggler").style.transform="rotate3ds(640sdeg)";
-  },[]);
+    document.getElementById("toggler").style.transform = "rotate3ds(640sdeg)";
+  }, []);
   window.onbeforeunload = closingCode;
   function closingCode() {
     document.getElementById("toggler").style.display = "block";
@@ -29,28 +29,28 @@ export default function Home() {
 
   return (
     <div className="m-0 p-0 d-flex justify-content-center embed-responsive embed-responsive-16by9 align-items-center vw-100 w-100 h-100 vh-100 " >
-    
-{/* //     style={{ */}
-{/* //       backgroundImage: `url("https://pagina-mama.s3.amazonaws.com/assets2/daniel/desert.png")`   */}
-{/* // }}> */}
-  
-    <div className="mask "style={{backgroundColor: 'rgba(0, 0, 0, 0.6)'}}>
+
+      {/* //     style={{ */}
+      {/* //       backgroundImage: `url("https://pagina-mama.s3.amazonaws.com/assets2/daniel/desert.png")`   */}
+      {/* // }}> */}
+
+      <div className="mask " style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
         {/* <div className="float-end">
           <img width={200} className=" p-0 m-0 animate__animated animate__zoomIn animate__slower z-3 bg-transparent shadow-0" src={"https://pagina-mama.s3.amazonaws.com/assets2/daniel/sign.png"} />
         </div> */}
-    </div>
-      <video
-        controls={false}
-        controlsList="nodownload noplayback"
-        autoPlay
-        muted
-        loop
-        
-        id="video-home"
-        className="  well embed-responsive-item animate__animated animate__slower animate__infinite"
-        allowFullScreen
-        src="https://pagina-mama.s3.amazonaws.com/assets2/daniel/Rough+Reel.mp4"
-      />
+      </div>
+      <div id="container-tv" className="position-fixed w-50 left-50 right-auto">
+        <div id="monitor" className="">
+          <div id="monitor-screen"  className="ratio ratio-16x9">
+              <iframe
+                src="https://pagina-mama.s3.amazonaws.com/assets2/daniel/Rough+Reel.mp4"
+                title="YouTube video"
+                allowfullscreen
+              ></iframe>
+            </div>
+            
+        </div>
+      </div>
     </div>
   );
 }
