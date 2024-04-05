bf48a624fcf4a8d7aebe456ae3d5fc6f720d2598
Donato Manzione <donato.manz.sc@gmail.com>
2024-04-04 23:01:27 -0400

    m

--- components/Templates/FilmTemplate.jsx
+++ components/Templates/FilmTemplate.jsx
@@ -22,57 +22,88 @@ export default function FilmTemplate(params) {
         vid.play();
     };
     return (
-
-        <>
-            <div className={"embed-responsive embed-responsive-16by9 vh-auto mh-100 mw-100 vw-100"} style={{backgroundColor: `${(basicModal)?"rgb(0,0,0)":""}`}}>
-                {window.screen.width < 768 &&
-
-                    <>   {basicModal && <video className="embed-responsive-item justify-content-center" playsInline={true} controls={true} autoPlay={false} src="https://pagina-mama.s3.amazonaws.com/assets2/daniel/Three+Bullets+to+Bombay+Beach/FullLength.mp4" alt="Three Bullets for Bombay" title="Three Bullets to Bombay Beach" style={{width: "100vw",display: "flex",height: "auto",maxHeight: "50vh",minHeight: "50vh"}}></video>
+        <div className="">
+            <div className="jumbotron jumbotron-fluid embed-responsive embed-responsive-16by9 px-50 mx-auto bg-black">
+                {(window.screen.width < 768) &&
+                    <>{basicModal && <video className="embed-responsive-item" autoPlay={false} src="https://pagina-mama.s3.amazonaws.com/assets2/daniel/Three+Bullets+to+Bombay+Beach/FullLength.mp4" alt="Three Bullets for Bombay" title="Three Bullets to Bombay Beach" id="video-film-page"></video>
                         ||
-                        <img src={still} alt={`#{title} Still Image`} className="mh-100 mw-100 vw-100 vh-auto img-fluid embed-responsive-item" />
-
-                    }
-                    </>
-                    ||
-                    <div style={{ display: (basicModal) ? "flex" : "none" }} className="embed-responsive embed-responsive-16by9 object-fit-cover" width="fit-content" height="100%">
-                        {basicModal && <iframe style={{ display: (basicModal) ? "flex" : "none" }} className="embed-responsive-item" autoPlay="false" src={fullLengthVideo} alt="Three Bullets for Bombay" title="Three Bullets to Bombay Beach" ></iframe>
-                            ||
-                            <img src={still} alt={`#{title} Still Image`} className="img-fluid mx-auto d-block mw-50 min-vw-50 w-lg-50 h-lg-50 w-sm-100 h-sm-100 w-xl-50 h-xl-50" />
-
-                        }
-
-                    </div>
-                }
-                {/* <video autoPlay loop style={{display:(basicModal)?"block":"none"}} src={clip} width="100%" height="100%" alt={title}/> */}
-                {/* <div className="mask bg-dark bg-opacity-25 "></div> */}
-                {basicModal === false &&
-                    <MDBBtn onClick={toggleOpen} id="playbutton" className="play-btn hover-overlay shadow-1 hover bg-image hover-overlay ripple shadow-1-strong rounded" >
-                        <svg enableBackground="new 0 0 141.73 141.73"
-                            height="141.73px"
-                            id="Warstwa_1"
-                            version="1.1"
-                            viewBox="0 0 141.73 141.73"
-                            width="141.73px"
-                            xmlSpace="preserve"
-                            xmlns="http://www.w3.org/2000/svg"
-                            xmlnsXlink="http://www.w3.org/1999/xlink">
-                            <g>
-                                <path d="M101.628,40.092c-8.22-8.22-19.149-12.746-30.774-12.746c-11.624,0-22.553,4.526-30.772,12.746
+                        (window.screen.width < 768 && !basicModal) &&
+                        <div>
+                        <img src={still} alt={`#{title} Still Image`} className="img-fluid" />
+                        <MDBBtn onClick={toggleOpen} id="playbutton" className="play-btn hover-overlay shadow-1 hover bg-image hover-overlay ripple shadow-1-strong rounded" >
+                            <svg enableBackground="new 0 0 141.73 141.73"
+                                height="141.73px"
+                                id="Warstwa_1"
+                                version="1.1"
+                                viewBox="0 0 141.73 141.73"
+                                width="141.73px"
+                                xmlSpace="preserve"
+                                xmlns="http://www.w3.org/2000/svg"
+                                xmlnsXlink="http://www.w3.org/1999/xlink">
+                                <g>
+                                    <path d="M101.628,40.092c-8.22-8.22-19.149-12.746-30.774-12.746c-11.624,0-22.553,4.526-30.772,12.746
         c-16.968,16.969-16.967,44.578,0.001,61.546c8.22,8.22,19.149,12.747,30.773,12.747s22.553-4.526,30.772-12.746
         s12.747-19.148,12.747-30.773S109.848,48.312,101.628,40.092z M100.214,100.225c-7.842,7.842-18.269,12.16-29.358,12.16
         s-21.517-4.319-29.359-12.161c-16.188-16.188-16.188-42.529-0.001-58.718c7.842-7.842,18.269-12.16,29.358-12.16
         c11.091,0,21.518,4.318,29.36,12.16c7.842,7.843,12.161,18.269,12.161,29.359S108.056,92.382,100.214,100.225z"
-                                    fill="#ffffff" />
-                                <path d="M65.893,55.983c-0.391-0.391-1.023-0.391-1.414,0s-0.391,1.023,0,1.414l13.466,13.466L64.478,84.331
+                                        fill="#ffffff" />
+                                    <path d="M65.893,55.983c-0.391-0.391-1.023-0.391-1.414,0s-0.391,1.023,0,1.414l13.466,13.466L64.478,84.331
         c-0.391,0.391-0.391,1.023,0,1.414c0.195,0.195,0.451,0.293,0.707,0.293s0.512-0.098,0.707-0.293L80.065,71.57
         c0.391-0.391,0.391-1.023,0-1.414L65.893,55.983z"
-                                    fill="#ffffff" />
-                            </g>
-                        </svg>
+                                        fill="#ffffff" />
+                                </g>
+                            </svg>
 
-                    </MDBBtn>
+                        </MDBBtn>
+                        </div>
+                    }</>
                 }
-                {/* <MDBModal open={basicModal} setOpen={setBasicModal} >
+
+                {(window.screen.width >= 768) &&
+
+                    <>{basicModal &&
+                        <video style={{ display: (basicModal) ? "flex" : "none" }} className="embed-responsive-item w-50 mx-auto " autoPlay="false" controls playsInline src={fullLengthVideo} alt="Three Bullets for Bombay" title="Three Bullets to Bombay Beach" ></video>
+                        || (window.screen.width >= 768 && !basicModal) &&
+                        <div>
+                        <img src={still} alt={`#{title} Still Image`} className="img-fluid vw-50 mx-auto" />
+                         <MDBBtn onClick={toggleOpen} id="playbutton" className="play-btn hover-overlay shadow-1 hover bg-image hover-overlay ripple shadow-1-strong rounded" >
+                            <svg enableBackground="new 0 0 141.73 141.73"
+                                height="141.73px"
+                                id="Warstwa_1"
+                                version="1.1"
+                                viewBox="0 0 141.73 141.73"
+                                width="141.73px"
+                                xmlSpace="preserve"
+                                xmlns="http://www.w3.org/2000/svg"
+                                xmlnsXlink="http://www.w3.org/1999/xlink">
+                                <g>
+                                    <path d="M101.628,40.092c-8.22-8.22-19.149-12.746-30.774-12.746c-11.624,0-22.553,4.526-30.772,12.746
+        c-16.968,16.969-16.967,44.578,0.001,61.546c8.22,8.22,19.149,12.747,30.773,12.747s22.553-4.526,30.772-12.746
+        s12.747-19.148,12.747-30.773S109.848,48.312,101.628,40.092z M100.214,100.225c-7.842,7.842-18.269,12.16-29.358,12.16
+        s-21.517-4.319-29.359-12.161c-16.188-16.188-16.188-42.529-0.001-58.718c7.842-7.842,18.269-12.16,29.358-12.16
+        c11.091,0,21.518,4.318,29.36,12.16c7.842,7.843,12.161,18.269,12.161,29.359S108.056,92.382,100.214,100.225z"
+                                        fill="#ffffff" />
+                                    <path d="M65.893,55.983c-0.391-0.391-1.023-0.391-1.414,0s-0.391,1.023,0,1.414l13.466,13.466L64.478,84.331
+        c-0.391,0.391-0.391,1.023,0,1.414c0.195,0.195,0.451,0.293,0.707,0.293s0.512-0.098,0.707-0.293L80.065,71.57
+        c0.391-0.391,0.391-1.023,0-1.414L65.893,55.983z"
+                                        fill="#ffffff" />
+                                </g>
+                            </svg>
+
+                        </MDBBtn>
+                        </div>
+                    }
+                    </>
+                }
+
+
+
+            </div>
+            {/* <video autoPlay loop style={{display:(basicModal)?"block":"none"}} src={clip} width="100%" height="100%" alt={title}/> */}
+            {/* <div className="mask bg-dark bg-opacity-25 "></div> */}
+     
+           
+            {/* <MDBModal open={basicModal} setOpen={setBasicModal} >
                     <MDBModalDialog>
                         
                         <div className="ratio ratio-16x9">
@@ -90,7 +121,7 @@ export default function FilmTemplate(params) {
                       
                     </MDBModalDialog>
                 </MDBModal> */}
-            </div>
+
             <MDBTable
                 className="vh-50  w-100 m-0 bg-white"
                 border={0}
@@ -107,57 +138,57 @@ export default function FilmTemplate(params) {
                         <p className="m-sm-0 p-sm-0 ms-sm-2 mx-lg-5 px-lg-5 py-2 my-2 ms-5 fs-5">{blurb}</p>
                     </div>
                 }
-            <MDBTableBody className="bg-white">
-                <tr className="d-flex flex-row justify-content-evenly">
-                    <td className="">
-                        <dt className="text-muted ">Release Date</dt>
-                        <dd>{date}</dd>
-                    </td>
-                    <td>
-                        <dt className="text-muted">Director</dt>
-                        <dd>Daniel Grzywacz</dd>
-                    </td>
-                    <td>
-                        <dt className={"text-muted"}>Starring</dt>
-                        <ul className="list-unstyled">
-                            {cast.map((c) =>
-                                (<li key={cast.indexOf(c)}>{`${c.castMember}`}<small><em> as </em><span className="ms-1"> {c.role}</span></small></li>))}
+                <MDBTableBody className="bg-white">
+                    <tr className="d-flex flex-row justify-content-evenly">
+                        <td className="">
+                            <dt className="text-muted ">Release Date</dt>
+                            <dd>{date}</dd>
+                        </td>
+                        <td>
+                            <dt className="text-muted">Director</dt>
+                            <dd>Daniel Grzywacz</dd>
+                        </td>
+                        <td>
+                            <dt className={"text-muted"}>Starring</dt>
+                            <ul className="list-unstyled">
+                                {cast.map((c) =>
+                                    (<li key={cast.indexOf(c)}>{`${c.castMember}`}<small><em> as </em><span className="ms-1"> {c.role}</span></small></li>))}
 
-                        </ul>
-                    </td>
-                    <td>
-                        <dt className={"text-muted"}>Runtime</dt>
-                        <dd>{`${runtime.value} ${runtime.unit ? runtime.unit : runtime.units}`}</dd>
-                    </td>
-                </tr>
+                            </ul>
+                        </td>
+                        <td>
+                            <dt className={"text-muted"}>Runtime</dt>
+                            <dd>{`${runtime.value} ${runtime.unit ? runtime.unit : runtime.units}`}</dd>
+                        </td>
+                    </tr>
 
 
 
-            </MDBTableBody>
+                </MDBTableBody>
 
-            <MDBRow>
-                {window.screen.width > 700 &&
+                <MDBRow>
+                    {window.screen.width > 700 &&
+                        <MDBCol>
+                        </MDBCol>
+                    }
                     <MDBCol>
-                    </MDBCol>
-                }
-                <MDBCol>
-                    <MDBBtn color="muted" className="border-0 shadow-0 outline-0 d-flex text-center flex-row justify-content-center" onClick={toggleInfo}>Peek into Daniel's process&nbsp;&nbsp;<span className="text-muted fw-light"><i className="fa fa-caret-down" aria-hidden="true"></i></span></MDBBtn>
+                        <MDBBtn color="muted" className="border-0 shadow-0 outline-0 d-flex text-center flex-row justify-content-center" onClick={toggleInfo}>Peek into Daniel's process&nbsp;&nbsp;<span className="text-muted fw-light"><i className="fa fa-caret-down" aria-hidden="true"></i></span></MDBBtn>
 
-                    <MDBCollapse open={showInfo} className='mt-3'>
+                        <MDBCollapse open={showInfo} className='mt-3'>
 
-                        {process.map(p => {
-                            return (<p key={process.indexOf(p)} className="text-justify text-muted p-2" style={{ textIndent: '1em' }}>
-                                {p}
-                            </p>);
-                        })}
-                    </MDBCollapse>
-                </MDBCol>
-                {window.screen.width > 700 &&
-                    <MDBCol>
+                            {process.map(p => {
+                                return (<p key={process.indexOf(p)} className="text-justify text-muted p-2" style={{ textIndent: '1em' }}>
+                                    {p}
+                                </p>);
+                            })}
+                        </MDBCollapse>
                     </MDBCol>
-                }
-            </MDBRow>
-        </MDBTable >
-        </>
+                    {window.screen.width > 700 &&
+                        <MDBCol>
+                        </MDBCol>
+                    }
+                </MDBRow>
+            </MDBTable >
+        </div>
     );
 }
